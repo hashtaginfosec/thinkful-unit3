a5b92d4208b510a614d2890d4ea5ba1f42825658
qasimchadhar <qasimchadhar@hotmail.com>
2014-07-28 03:02:35 -0500

    Commiting first attempt

---
+++ github_languages.py
@@ -0,0 +1,26 @@
+import sys
+import operator
+from collections import defaultdict
+import requests
+
+
+def get_repositories(user):
+    """
+    Retrieve a list of user's repositories
+    """
+    url = "https://api.github.com/users/{user}/repos".format(user=user)
+    response = requests.get(url)
+    return response.json()
+
+
+def main():
+    """
+    main function
+    """
+
+    repositories = get_repositories(sys.argv[1])
+    print(repositories)
+
+
+if __name__ == "__main__":
+    main()
