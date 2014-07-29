e8874f179f84d53f54b113fcfd0c1d462db5ff7a
qasimchadhar <qasimchadhar@users.noreply.github.com>
2014-07-28 22:56:25 -0500

    Complete

--- github_languages.py
+++ github_languages.py
@@ -5,6 +5,17 @@ import requests
 from secret import USERNAME, PASSWORD
 
 
+def accumulate_languages(language_dictionaries):
+    """ Calculate the total data size for each langauge """
+    accumulated = defaultdict(int)
+    total = 0
+    for language_dictionary in language_dictionaries:
+        for language_name, number_of_bytes in language_dictionary.iteritems():
+            accumulated[language_name] += number_of_bytes
+            total += number_of_bytes
+    return accumulated, total
+
+
 def get_repositories(user):
     """
     Retrieve a list of user's repositories
@@ -14,13 +25,34 @@ def get_repositories(user):
     return response.json()
 
 
+def get_language_dictionaries(repositories):
+    """Return a list of dictionaries containing the languages used in each repo"""
+    language_dictionaries = []
+    for repository in repositories:
+        url = "https://api.github.com/repos/{owner}/{repo}/languages"
+        url = url.format(owner=repository["owner"]["login"], repo=repository["name"])
+        response = requests.get(url, auth=(USERNAME, PASSWORD))
+        language_dictionaries.append(response.json())
+    return language_dictionaries
+
+
 def main():
     """
     main function
     """
 
     repositories = get_repositories(sys.argv[1])
-    print(repositories)
+    #print(repositories)
+    language_dictionaries = get_language_dictionaries(repositories)
+    language_totals, total_bytes = accumulate_languages(language_dictionaries)
+
+    sorted_language_totals = sorted(language_totals.iteritems(),
+                                    key=operator.itemgetter(1),
+                                    reverse=True)
+
+    for language_name, number_of_bytes in sorted_language_totals:
+        percentage = 100.0 * number_of_bytes / total_bytes
+        print("{}: {:.2f}%".format(language_name, percentage))
 
 
 if __name__ == "__main__":
